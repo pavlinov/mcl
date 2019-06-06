b0105b6f9e30a0ea682b42ee1fb860dfc90de645
Alexey Pavlinov <apavlinov@geeksforless.net>
2019-06-06 19:13:59 +0300

    Simple webserver - infinity loop, HEADER Content-Length added

--- bash/simple.webserver.httpd.sh
+++ bash/simple.webserver.httpd.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-(echo -e "HTTP/1.1 200 OK\nContent-Type: text/html\n\n Hello World";) | nc -v -l 8888
+
+##############################################
+# Simple WebServer on Bash
+# httpd on bash
+#
+# example for Mykolaiv Classical Liceum
+##############################################
+
+echo "Web server started..."
+
+while true;
+	do
+	(
+		VARHTML=$(echo "Hello World!")
+		VARHTMLLENGTH=$(echo $VARHTML | wc -m)
+
+		echo -e "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: $VARHTMLLENGTH\n\n $VARHTML";
+	) | netcat -vv -l 8888;
+	done
