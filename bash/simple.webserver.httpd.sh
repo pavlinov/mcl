b36d25099ae99fd74ad18801a7975e3327214869
Alexey Pavlinov <apavlinov@geeksforless.net>
2019-06-06 19:27:11 +0300

    Simple web server small improvements

--- bash/simple.webserver.httpd.sh
+++ bash/simple.webserver.httpd.sh
@@ -5,7 +5,8 @@
 # httpd on bash                                                        #
 #                                                                      #
 # example for Mykolaiv Classical Liceum                                #
-# Copyright: (c) 2019 Mykolaiv Classical Liceum. All rights reserved.   #
+#                                                                      #
+# Copyright: (c) 2019 Mykolaiv Classical Liceum. All rights reserved.  #
 # Author: Alexey Pavlinov                                              #
 ########################################################################
 
@@ -17,6 +18,10 @@ while true;
 		VARHTML=$(cat simple.webserver.httpd.files/index.html) # `echo "Hello World!"` can be replaced to `cat index.html`
 		VARHTMLLENGTH=$(echo $VARHTML | wc -m)
 
-		echo -e "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: $VARHTMLLENGTH\n\n $VARHTML";
+		echo -ne "HTTP/1.1 200 OK\n";
+		echo -ne "Content-Type: text/html\n";
+		echo -ne "Content-Length: $VARHTMLLENGTH\n";
+		echo -ne "\n\n";
+		echo -ne $VARHTML;
 	) | netcat -vv -l 8888;
 	done
